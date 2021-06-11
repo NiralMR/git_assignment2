2b54f09e7cd09daa7853c21aeb9e884faef39d9f
NiralMR <niralkpatelia@gmail.com>
2021-06-11 23:31:53 +0530

    Add and diff files created

---
+++ add.java
@@ -0,0 +1,13 @@
+public class add{
+    int a;
+    int b;
+
+    public add(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+
+    public int addInt(){
+        return a+b;
+    }    
+}
---
+++ diff.java
@@ -0,0 +1,13 @@
+public class diff{
+    int a;
+    int b;
+
+    public diff(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+
+    public int diffInt(){
+        return a-b;
+    }    
+}
